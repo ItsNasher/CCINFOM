284875bc2ec0be8449854390f6c4bc58e4f9e1a6
DonD13 <122430679+DonD13@users.noreply.github.com>
2024-11-23 01:32:45 +0800

    Driver.java
    Added the Product, Inventory, Sales_Transac

--- src/petdaycare/Driver.java
+++ src/petdaycare/Driver.java
@@ -8,14 +8,16 @@ public class Driver {
         int selection = 0;
         Scanner sc = new Scanner(System.in);
 
-        while (selection != 4) {
+        while (selection != 7) {
 			System.out.println("=====================================================");
 			System.out.println("Welcome to our Pet Daycare!");
 			System.out.println ("[1] Manage Owner Records");
             		System.out.println ("[2] Manage Pet Records");
 			System.out.println ("[3] Enroll Student in a Course");
 			System.out.println ("[4] Manage Product Records");
-			System.out.println ("[5] Quit Application");
+			System.out.println ("[5] Manage Inventory Records");
+			System.out.println ("[6] Manage Sale Transaction Records");
+			System.out.println ("[7] Quit Application");
 			System.out.println("=====================================================");
 			System.out.print ("Enter function to perform: ");
 			selection = sc.nextInt();
@@ -23,7 +25,7 @@ public class Driver {
 	
 			if (selection==1) {
 				Owner o = new Owner();
-				while (o.function() != 5) {};
+				while (o.ownerMenu() != 7) {};
 			} /*else if (selection==2) {
 				enroll e = new enroll();
 				while (e.function() != 1) {}; 
@@ -31,10 +33,21 @@ public class Driver {
 				report_01 r = new report_01();
 				while (r.function() != 1) {};
 				*/
-			} else if (selection==4) {
+			else if (selection==4) {
 				Product p = new Product();
 				while (p.function() !=5) {};
 			} 
+			else if (selection==5){
+				Inventory i = new Inventory();
+				while (i.function() !=5) {};
+			}
+			else if (selection==6){
+				Sales_Transaction st = new Sales_Transaction();
+				while (st.function() !=5) {};
+			}
+			else if (selection==7){
+				System.out.println("Thank you for visiting our Pet Daycare!");
+			}
 		}
     }
 }
