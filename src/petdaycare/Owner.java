8a914f8a70cf5826ae77637dde2e9c189601fb2d
ethan <165283236+ItsNasher@users.noreply.github.com>
2024-11-22 20:39:31 +0800

    modify doesnt work yet

--- src/petdaycare/Owner.java
+++ src/petdaycare/Owner.java
@@ -7,50 +7,35 @@ public class Owner {
     public int Owner_ID;
     public String Owner_FirstName;
     public String Owner_LastName;
-    public int Contact_Info;
+    public String Contact_Info;
     public String City;
-    public String Street;
-    public int Postal_Code;
 
     // list of owner
     public ArrayList<Integer> Owner_IDlist = new ArrayList<>();
     public ArrayList<String> Owner_FirstNamelist = new ArrayList<>();
     public ArrayList<String> Owner_LastNamelist = new ArrayList<>();
-    public ArrayList<Integer> Contact_Infolist = new ArrayList<>();
+    public ArrayList<String> Contact_Infolist = new ArrayList<>();
     public ArrayList<String> Citylist = new ArrayList<>();
-    public ArrayList<String> Streetlist = new ArrayList<>();
-    public ArrayList<Integer> Postal_Codelist = new ArrayList<>();
 
-    public Owner() {
+    public Owner() {}
 
-    }
-    public int register_owner(){
+    public void register_owner(){
         String url = "jdbc:mysql://localhost:3310/db_app_services";
         String username = "root";
         String password = "ethan";
+        Connection conn = null;
         try { // jdbc:mysql://localhost:3310/db_app_services?useTimezone=true&serverTimezone=UTC&user=root
-            Connection conn;
             conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connection Succesful");
-            
-            /*Statement statement = conn.createStatement();
-            ResultSet resultSet =statement.executeQuery("SELECT * FROM Owner");
-
-            while (resultSet.next()){
-                System.out.println(resultSet.getString("First_Name"));
-                System.out.println(resultSet.getString("Last_Name"));
-            } */
-            //save the new asset, only 6 ? because id auto inrements
             PreparedStatement pstmt = conn.prepareStatement(
-            "INSERT INTO Owner (First_Name, Last_Name, Contact_Info, City, Street, Postal_Code) " +
-            "VALUES (?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            "INSERT INTO Owner (First_Name, Last_Name, Contact_Info, City) " +
+            "VALUES (?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
 
             pstmt.setString(1, Owner_FirstName);
             pstmt.setString(2, Owner_LastName);
-            pstmt.setInt(3, Contact_Info);
+            pstmt.setString(3, Contact_Info);
             pstmt.setString(4, City);
-            pstmt.setString(5, Street);
-            pstmt.setInt(6, Postal_Code);
+   
 
             int added = pstmt.executeUpdate();
             if (added > 0){
@@ -62,24 +47,103 @@ public class Owner {
                 Owner_ID = generatedKeys.getInt(1);
                 System.out.println("Generated Owner_ID: " + Owner_ID);
             }
-            return 1; 
         } catch (Exception e){
             System.out.println(e.getMessage());
-            return 0;
         }
-        
+        if (conn != null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            };
+        }
     }
 
-    public int function() {
-		/* this function is to provide an I/O module to facilitate the different record functions
-		 * of the class. The function can be called by the MAIN MODULE of the DB Application.
-		 */
+    public void viewOwner(){
+        String url = "jdbc:mysql://localhost:3310/db_app_services";
+        String username = "root";
+        String password = "ethan";
+        Connection conn = null;
+
+        try { // jdbc:mysql://localhost:3310/db_app_services?useTimezone=true&serverTimezone=UTC&user=root
+            conn = DriverManager.getConnection(url, username, password);            
+            Statement statement = conn.createStatement();
+            ResultSet resultSet =  statement.executeQuery("SELECT * FROM Owner");
+
+            System.out.printf("%-10s %-15s %-15s %-15s %-15s\n",
+            "ID", "First Name", "Last Name", "Phone", "City");
+            System.out.println("----------------------------------------------------------------------------");
+
+            // Data Rows
+            while (resultSet.next()) {
+                int id = resultSet.getInt("Owner_ID");
+                String firstName = resultSet.getString("First_Name");
+                String lastName = resultSet.getString("Last_Name");
+                String phone = resultSet.getString("Contact_Info");
+                String city = resultSet.getString("City");
+
+                // Ensure consistent spacing for output
+                System.out.printf("%-10d %-15s %-15s %-15s %-15s\n",
+                        id, firstName, lastName, phone, city);
+            } 
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+        if (conn != null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            };
+        }
+
+    }
+
+    public void modifyOwner(){
+        Scanner sc = new Scanner(System.in);
+		PreparedStatement pstmt;
+        String url = "jdbc:mysql://localhost:3310/db_app_services";
+        String username = "root";
+        String password = "ethan";
+		Connection conn = null;
+		try {
+            
+            conn = DriverManager.getConnection(url, username, password);
+			pstmt = conn.prepareStatement("UPDATE Owner SET First_Name=?, Last_Name=?, Contact_Info=?, City=? WHERE Owner_ID=?");
+			
+			pstmt.setString(1, Owner_FirstName);
+			pstmt.setString(2, Owner_LastName);
+			pstmt.setString(3, Contact_Info);
+            pstmt.setString(4, City);
+            pstmt.setInt(5, Owner_ID);
+			
+			pstmt.executeUpdate();
+			pstmt.close();
+			System.out.println("Student record updated in the Database");			
+		} catch (Exception e) {
+	        System.out.println("Error occured while updating a Student Record");
+	        System.out.println(e.getMessage());	
+		}
+        if (conn != null){
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            };
+        }
+    }
+
+    public void removeOwner(){
+
+    }
+
+    public int ownerMenu() {
 		Scanner sc 	= new Scanner(System.in);		
 		System.out.println ("------------------------------------------------------");
 		System.out.println ("What would you like to do with the Owner Records?");
 		System.out.println ("[1] - Register a new Owner Record");
 		System.out.println ("[2] - View an Owner Record");
-		System.out.println ("[3] - Update an Owner Record");
+		System.out.println ("[3] - Modify an Owner Record");
 		System.out.println ("[4] - Delete an Owner Record");
 		System.out.println ("[5] - Exit");
 		System.out.print ("Enter number to perform: ");
@@ -87,74 +151,81 @@ public class Owner {
         sc.nextLine();
 		
 		if (selection == 1) {
-			/* Mechanics for Creating a Student Record 
-			 * 1. Ask user for the data about the student
-			 * 2. Create the new record
-			 */
-             
 			System.out.println("Creating new record of a Student");
-            System.out.println("Enter First Name: ");
+            System.out.print("Enter First Name: ");
             Owner_FirstName = sc.nextLine();
 
-            System.out.println("Enter Last Name: ");
+            System.out.print("Enter Last Name: ");
             Owner_LastName = sc.nextLine();
 
-            System.out.println("Enter Contact Info: ");
-            Contact_Info = sc.nextInt();
-            sc.nextLine(); // Consume newline
+            System.out.print("Enter Contact Info: ");
+            Contact_Info = sc.nextLine();
 
-            System.out.println("Enter City: ");
+            System.out.print("Enter City: ");
             City = sc.nextLine();
-
-            System.out.println("Enter Street: ");
-            Street = sc.nextLine();
-
-            System.out.println("Enter Postal Code: ");
-            Postal_Code = sc.nextInt();
-            sc.nextLine();
 
 			register_owner(); 
 		}  
-       /* else if (selection == 2) {
-			/* Mechanics for Retrieving Student Record 
-			 * 1. Ask user for student ID
-			 * 2. Retrieve the record with the student ID provided
-			 
-			System.out.println("Enter student ID to retrieve :");
-			studentid = sc.nextInt();
-			viewStudent();			
+        else if (selection == 2) {
+			viewOwner();		
 		}  else if (selection == 3) {
-			// Mechanics for Updating a Record
-			// 1. Ask for the identifier of the record
-			// 2. Retrieve the old data of the record
-			// 3. Ask for the new set of data of the record. 
-			//    if user did not put any value then old data will be used.
-			// 4. Call the update function
             
-
-			System.out.println("Enter student ID to update :");
-			studentid = sc.nextInt();
+            viewOwner();
+			System.out.print("Enter student ID to update :");
+			Owner_ID = sc.nextInt();
 			sc.nextLine();
-			viewStudent();
-			
-			if (norecord) {
-				System.out.println("No record to update.");
-			} else {
-				String newlastname  = new String();
-				String newfirstname = new String();
-				System.out.println ("Enter updated Last Name: ");
-				newlastname  = sc.nextLine();
-				System.out.println ("Enter updated First Name: ");
-				newfirstname = sc.nextLine();
-	
-				// Check if the field was left not to be updated
-				// If so, then use the old value of the data
-				 
-				if (newlastname.isEmpty()  == false) lastname = newlastname;
-				if (newfirstname.isEmpty() == false) firstname = newfirstname;
-				updateStudent();
-			} 
-		} else if (selection == 4) {
+
+            String url = "jdbc:mysql://localhost:3310/db_app_services";
+            String username = "root";
+            String password = "ethan";
+
+            String newLastname  = null;
+            String newFirstname = null;
+            String newContactInfo = null;
+            String newCity = null;
+            try {
+                Connection conn = DriverManager.getConnection(url, username, password);
+                
+                // Check if the ID exists
+                PreparedStatement checkStmt = conn.prepareStatement("SELECT * FROM Owner WHERE Owner_ID = ?");
+                checkStmt.setInt(1, Owner_ID);
+                ResultSet resultSet = checkStmt.executeQuery();
+                
+                if (!resultSet.next()) {
+                    System.out.println("No record found with Owner ID: " + Owner_ID);
+                } else {
+                    System.out.print("Enter updated Last Name (Enter if no changes): ");
+                    newLastname = sc.nextLine();
+                    System.out.print("Enter updated First Name (Enter if no changes): ");
+                    newFirstname = sc.nextLine();
+                    System.out.print("Enter updated Contact Info (Enter if no changes): ");
+                    newContactInfo = sc.nextLine();
+                    System.out.print("Enter updated City (Enter if no changes): ");
+                    newCity = sc.nextLine();
+
+                    if (newLastname != null && !newLastname.isEmpty()){
+                        Owner_LastName = newLastname;
+                    }
+
+                    if (newFirstname != null && !newFirstname.isEmpty()){
+                        Owner_FirstName = newFirstname;
+                    }
+
+                    if (newContactInfo != null && !newContactInfo.isEmpty()){
+                        Contact_Info = newContactInfo;
+                    } 
+                    if (newCity != null && !newCity.isEmpty()){
+                        City = newCity;
+                    }
+
+                    modifyOwner();
+                }
+            } catch (Exception e){
+                System.out.println(e.getMessage());
+            }   
+        }
+         
+            /*else if (selection == 4) {
 			// Mechanics for Deleting a Record
 			// 1. Ask for the identifier of the record
 			// 2. Retrieve the old data of the record
@@ -187,7 +258,7 @@ public class Owner {
 	}
     public static void main(String[] args) {
         Owner o = new Owner();
-        while (o.function() != 5){
+        while (o.ownerMenu() != 5){
         };
     }
 }
