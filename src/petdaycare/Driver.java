aaa2312e51c559237c9bc789def116cab4143cab
ethan <165283236+ItsNasher@users.noreply.github.com>
2024-11-23 11:46:55 +0800

    fixed

--- src/petdaycare/Driver.java
+++ src/petdaycare/Driver.java
@@ -13,7 +13,7 @@ public class Driver {
 			System.out.println("Welcome to our Pet Daycare!");
 			System.out.println ("[1] Manage Owner Records");
             System.out.println ("[2] Manage Pet Records");
-			System.out.println ("[3] Enroll Student in a Course");
+			System.out.println ("[3] Manage Service Records");
 			System.out.println ("[4] Manage Product Records");
 			System.out.println ("[5] Manage Inventory Records");
 			System.out.println ("[6] Manage Sale Transaction Records");
