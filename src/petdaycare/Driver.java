b04c4212b200251fce2c3c1c4b068eaa60ae1654
Ethan Iago Dela Cruz <165283236+ItsNasher@users.noreply.github.com>
2024-11-23 15:02:51 +0800

    added services part

--- src/petdaycare/Driver.java
+++ src/petdaycare/Driver.java
@@ -12,7 +12,7 @@ public class Driver {
 			System.out.println("=====================================================");
 			System.out.println("Welcome to our Pet Daycare!");
 			System.out.println ("[1] Manage Owner Records");
-            System.out.println ("[2] Manage Pet Records");
+            		System.out.println ("[2] Manage Pet Records");
 			System.out.println ("[3] Manage Service Records");
 			System.out.println ("[4] Manage Product Records");
 			System.out.println ("[5] Manage Inventory Records");
@@ -29,10 +29,11 @@ public class Driver {
 			} /*else if (selection==2) {
 				enroll e = new enroll();
 				while (e.function() != 1) {}; 
-			} else if (selection==3) {
-				report_01 r = new report_01();
-				while (r.function() != 1) {};
-				*/
+    			}*/
+			else if (selection==3) {
+				Services s = new Services();
+				while (s.serviceMenu() != 5) {}
+			}
 			else if (selection==4) {
 				Product p = new Product();
 				while (p.function() !=5) {};
